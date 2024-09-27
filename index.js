a24974d48e1c66b19464c40bd85266b7aeb067ab
Yevhen <e.v.ilnytskyi@gmail.com>
2024-09-27 16:36:33 +0300

    real bk

--- index.js
+++ index.js
@@ -15,24 +15,24 @@ const api = new WooCommerceRestApi({
   version: "wc/v3",
 });
 
-app.use((req, res, next) => {
-  const allowedOrigins = [
-    "http://localhost:3000",
-    "http://bike-ecommerce:3000",
-    "http://192.168.2.239:3000",
-    "http://bikeecommerce.atwebpages.com",
-  ];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+// app.use((req, res, next) => {
+//   const allowedOrigins = [
+//     "http://localhost:3000",
+//     "http://bike-ecommerce:3000",
+//     "http://192.168.2.239:3000",
+//     "http://bikeecommerce.atwebpages.com",
+//   ];
+//   const origin = req.headers.origin;
+//   if (allowedOrigins.includes(origin)) {
+//     res.setHeader("Access-Control-Allow-Origin", origin);
+//   }
+//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
 
-app.use(cors());
-app.use(bodyParser.json());
+// app.use(cors());
+// app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("API is running!");
