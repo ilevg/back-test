254f4dad3c8801559a4c5e05a47ee61db117b3c5
Yevhen <e.v.ilnytskyi@gmail.com>
2024-09-27 20:59:49 +0300

    b

--- index.js
+++ index.js
@@ -15,71 +15,9 @@ const api = new WooCommerceRestApi({
 app.get("/", (req, res) => {
   res.send("API is running!");
 });
-app.use(cors());
-app.use(bodyParser.json());
-app.get("/products", async (req, res) => {
-  try {
-    const { data } = await api.get("products", { per_page: 50 });
-    res.json(data);
-  } catch (error) {
-    res
-      .status(error.response.status)
-      .json({ error: error.response.statusText });
-    console.error("Error fetching products:", error.message);
-  }
-});
 
-app.get("/customers?:id", async (req, res) => {
-  try {
-    const { data } = await api.get("customers");
-    res.json(data);
-  } catch (error) {
-    res
-      .status(error.response.status)
-      .json({ error: error.response.statusText });
-    console.error("Error fetching customers:", error.message);
-  }
-});
-
-app.post("/orders", async (req, res) => {
-  try {
-    const orderData = req.body;
-    const isHeadlessCMS = req.headers["x-headless-cms"];
-    if (isHeadlessCMS && isHeadlessCMS.toLowerCase() === "true") {
-      const { data } = await api.post("orders", orderData);
-      res.json(data);
-    } else {
-      throw new Error("Unauthorized");
-    }
-  } catch (error) {
-    res.status(401).json({ error: "Unauthorized" });
-    console.error("Error creating order:", error.message);
-  }
-});
-
-app.post("/register", async (req, res) => {
-  const { email, pass } = req.body;
-  const authKey = process.env.AUTH_KEY;
-  const wordpressUrl = process.env.WORDPRESS_SITE_URL;
-  try {
-    const registerUrl = `${wordpressUrl}/?rest_route=/simple-jwt-login/v1/users&email=${email}&password=${pass}&AUTH_KEY=${authKey}`;
-    const registerResponse = await axios.post(registerUrl);
-    const registerData = registerResponse.data;
-    console.log(registerData);
-
-    if (registerData.success === true) {
-      const authUrl = `${wordpressUrl}/?rest_route=/simple-jwt-login/v1/auth&email=${email}&password=${pass}&AUTH_KEY=${authKey}`;
-      const authResponse = await axios.post(authUrl);
-
-      res.json(authResponse.data);
-    } else {
-      return res.json(registerData);
-    }
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-    console.error("Error during registration/authentication:", error.message);
-  }
-});
+// app.use(cors());
+// app.use(bodyParser.json());
 
 app.listen(process.env.PORT || 4000, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
