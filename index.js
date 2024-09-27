236f87814d5863126e4e67c7d494c9713f86b9df
Yevhen <e.v.ilnytskyi@gmail.com>
2024-09-27 21:06:26 +0300

    bq

--- index.js
+++ index.js
@@ -5,19 +5,11 @@ const axios = require("axios");
 
 require("dotenv").config();
 
-// const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
-// const api = new WooCommerceRestApi({
-//   url: process.env.WORDPRESS_SITE_URL,
-//   consumerKey: process.env.WC_CONSUMER_KEY,
-//   consumerSecret: process.env.WC_CONSUMER_SECRET,
-//   version: "wc/v3",
-// });
+const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
+const app = express();
 app.get("/", (req, res) => {
   res.send("API is running!");
 });
-
-// app.use(cors());
-// app.use(bodyParser.json());
 
 app.listen(process.env.PORT || 4000, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
