78d0862da6d22ca6acdd0a616bec11bee7a7d168
Yevhen <e.v.ilnytskyi@gmail.com>
2024-09-27 21:10:15 +0300

    brt

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+const api = new WooCommerceRestApi({
+  url: process.env.WORDPRESS_SITE_URL,
+  consumerKey: process.env.WC_CONSUMER_KEY,
+  consumerSecret: process.env.WC_CONSUMER_SECRET,
+  version: "wc/v3",
+});
+
 app.get("/", (req, res) => {
   res.send("API is running!");
 });
